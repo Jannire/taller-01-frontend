92973971cd5b7cf96746a0ed36f7b5c231a17e3f
Jannire Trujillo <Janniretruji@gmail.com>
2022-10-27 22:40:15 -0500

    arreglar detalles

--- src/screens/ListadoCiclos/ListadoCiclosPage.jsx
+++ src/screens/ListadoCiclos/ListadoCiclosPage.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react"
-import {Container} from 'react-bootstrap'
+import {Container, Row, Col, FormText} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
 import Layout from "../../components/Layout"
+import GridCursos from "../ListadoCursos/components/GridCursos"
 import GridCiclos from "./components/GridCiclos"
+import FiltroCarrera from "../ListadoCursos/components/FiltroCarrera"
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const ListadoCiclosPage = () => {
     const [listadoCiclos, setListadoCiclos] = useState([])
+    const [listadoCarreras, setListadoCarreras] = useState([])
+    const [listadoCursos, setListadoCursos] = useState([])
 
     const httpObtenerCiclos = async () => {
         
@@ -16,16 +23,60 @@ const ListadoCiclosPage = () => {
         setListadoCiclos(data)
     }
 
+    const httpObtenerCarreras = async () => {
+        const resp = await fetch("http://localhost:4444/carreras")
+        const data = await resp.json()
+        console.log(data)
+        setListadoCarreras(data)
+    }
+
+    const httpObtenerCursos = async (carreraId = null) => {
+        const ruta = carreraId == null ? 
+            "http://localhost:4444/cursos" : 
+            `http://localhost:4444/cursos?carrera=${carreraId}`
+
+        const resp = await fetch(ruta)
+        const data = await resp.json()
+        console.log(data)
+        setListadoCursos(data)
+    }
+
     useEffect(() => {
         httpObtenerCiclos()
     }, [])
 
+    useEffect(() => {
+        httpObtenerCarreras()
+        httpObtenerCursos()
+    }, [])
+
+    const onCarreraSelected = (carreraId) => {
+        console.log("Se selecciono carrera " +  carreraId)
+        httpObtenerCursos(carreraId)
+    }
+
     return <Layout
-        makeHeader={ () => <Header titulo="Listado de Ciclos" /> }
+        makeHeader={ () => <Header titulo="Listado de Ciclos y Cursos" /> }
         makeBody={ 
-            () =>  <div>
-                <GridCiclos ciclos={ listadoCiclos }/>
-            </div>
+            () =>  <Row>
+                <Col>
+                    <h2>Ciclos</h2>
+                    <InputGroup className="mb-3">
+                    <Form.Control placeholder="Ciclo" aria-label="Ciclo" aria-describedby="basic-addon2"/>
+                    <Button variant="outline-secondary" id="button-addon2">
+                        +
+                    </Button>
+        </InputGroup>
+                    <GridCiclos className="col" ciclos={ listadoCiclos }/>
+                </Col>
+                <Col>
+                    <h2>Cursos</h2>
+                    <FiltroCarrera
+                        carreras={ listadoCarreras }
+                        onCarreraSelected={ onCarreraSelected }/>
+                    <GridCursos cursos={ listadoCursos }/>
+                </Col>
+            </Row>
         }
         makeFooter={ () => <Footer /> }
     />
